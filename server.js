b35b8daa734722b960ff16489361d769eb67dca2
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-16 10:12:24 -0400

    Pueba

--- server.js
+++ server.js
@@ -36,8 +36,14 @@ const authRoutes = require('./routes/auth');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Configuración de CORS
+const corsOptions = {
+  origin: ['https://ferreteria-frontend.vercel.app'], // URL de tu frontend
+  credentials: true
+};
+app.use(cors(corsOptions));
+
 // Middleware
-app.use(cors());
 app.use(bodyParser.json());
 
 // Servir archivos estáticos
