4e72297adf6be484754d38b8b0c61d420717c12d
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-21 20:17:36 -0400

    agg correcciones clientes y ventas

--- Routes/ventas.js
+++ Routes/ventas.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose'); 
 const router = express.Router();
 const mongoose = require('mongoose');
 const Venta = require('../models/Venta');
