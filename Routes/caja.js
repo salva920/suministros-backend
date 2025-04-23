5530aadb9f50393a7502bb2251b33885637370fc
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-23 14:21:07 -0400

    prueba put clientes

--- Routes/caja.js
+++ Routes/caja.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Caja = require('../models/caja');
-const Transaccion = require('../models/transaccion');
 const moment = require('moment-timezone');
 
 // En routes/caja.js
