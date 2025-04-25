3ea5dc4b5997709d8dac6148500e7d3d4123076d
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-25 12:35:38 -0400

    agg modulo listado de precios

--- models/ListaPrecio.js
+++ models/ListaPrecio.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
 const Schema = mongoose.Schema;
 
 const listaPrecioSchema = new Schema({
@@ -43,11 +44,42 @@ const listaPrecioSchema = new Schema({
   fechaActualizacion: {
     type: Date,
     default: Date.now
+  },
+  margenGanancia1: {
+    type: Number,
+    default: 0
+  },
+  margenGanancia2: {
+    type: Number,
+    default: 0
+  },
+  margenGanancia3: {
+    type: Number,
+    default: 0
+  },
+  margenGananciaMayorista: {
+    type: Number,
+    default: 0
   }
 }, { timestamps: true });
 
 // Índices para optimizar búsquedas
 listaPrecioSchema.index({ producto: 1 }, { unique: true });
 listaPrecioSchema.index({ codigoProducto: 1 });
+listaPrecioSchema.index({ nombreProducto: 'text' }); // Índice de texto para búsquedas
+
+// Método para calcular márgenes de ganancia
+listaPrecioSchema.methods.calcularMargenes = async function(costoProd) {
+  const costo = costoProd || 0;
+  if (costo > 0) {
+    this.margenGanancia1 = costo > 0 ? ((this.precio1 - costo) / costo) * 100 : 0;
+    this.margenGanancia2 = costo > 0 ? ((this.precio2 - costo) / costo) * 100 : 0;
+    this.margenGanancia3 = costo > 0 ? ((this.precio3 - costo) / costo) * 100 : 0;
+    this.margenGananciaMayorista = costo > 0 ? ((this.precioMayorista - costo) / costo) * 100 : 0;
+  }
+};
+
+// Agregar plugin de paginación
+listaPrecioSchema.plugin(mongoosePaginate);
 
 module.exports = mongoose.model('ListaPrecio', listaPrecioSchema);
