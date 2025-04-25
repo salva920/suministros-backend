bbde6261201eac5ea82bf53d80743cdb9935363b
Carmenuvm <cmcn5toe2017@gmail.com>
2025-04-25 14:04:00 -0400

    agg modulo listado de precios

--- server.js
+++ server.js
@@ -69,7 +69,7 @@ app.use('/api/ventas', require('./Routes/ventas'));
 
 app.use('/api/facturaPendiente', require('./Routes/FacturaPendiente'));
 
-app.use('/api/listaprecios', require('./Routes/ListaPrecio'));
+app.use('/api/listaprecios', require('./Routes/listaPrecio'));
 
 // Configurar CORS para permitir solicitudes desde el frontend
 app.use((req, res, next) => {
